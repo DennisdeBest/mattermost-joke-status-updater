5dff601675c2bee5b2238490d6e208c3d001f6fb
Dennis de Best <dennis@debest.fr>
2022-07-22 18:16:02 +0200

    Init builds

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-wget -c https://github.com/DennisdeBest/mattermost-joke-status-updater/releases/download/v0.0.9/mattermost-status-joke-updater-amd64 -O /usr/local/bin/mattermost-joke-status-updater
+wget -c https://github.com/DennisdeBest/mattermost-joke-status-updater/releases/latest/download/mattermost-joke-status-updater-amd64 -O /usr/local/bin/mattermost-joke-status-updater
 chmod +x /usr/local/bin/mattermost-joke-status-updater
